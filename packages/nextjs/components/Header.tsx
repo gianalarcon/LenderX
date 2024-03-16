2ddaa148d2d7155d5b3f098fea0264f62ae02b01
Steve James Foncy Arias Arias <stevejamesfoncyariasarias@MacBook-Pro-de-Steve.local>
2024-03-16 17:43:11 +0000

    feat: add dynamic auth service

--- packages/nextjs/components/Header.tsx
+++ packages/nextjs/components/Header.tsx
@@ -3,9 +3,10 @@
 import Image from "next/image";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
+import { DynamicWidget } from "@dynamic-labs/sdk-react-core";
 import { formatEther, parseEther } from "viem";
 import { useAccount } from "wagmi";
-import { FaucetButton, RainbowKitCustomConnectButton } from "~~/components/scaffold-eth";
+import { FaucetButton } from "~~/components/scaffold-eth";
 import { useScaffoldContractRead, useScaffoldContractWrite } from "~~/hooks/scaffold-eth";
 
 type HeaderMenuLink = {
@@ -92,7 +93,7 @@ export const Header = () => {
               Mint
             </button>
           </div>
-          <RainbowKitCustomConnectButton />
+          <DynamicWidget />
           <FaucetButton />
         </div>
       </div>

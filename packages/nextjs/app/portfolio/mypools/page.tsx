2e07b71df6ed428d5515fbccb467127f00cd0e3e
gianalarcon <gian.alarcon@unmsm.edu.pe>
2024-03-16 14:37:16 +0000

    Feat: Update compoents and add function mint

--- packages/nextjs/app/portfolio/mypools/page.tsx
+++ packages/nextjs/app/portfolio/mypools/page.tsx
@@ -1,11 +1,21 @@
 "use client";
 
 import { useEffect, useState } from "react";
+import Image from "next/image";
 import { formatUnits } from "viem";
 import { Address, useAccount, useContractReads } from "wagmi";
-import Cheap from "~~/components/Cheap/Cheap";
-import { data } from "~~/data/data";
+import LocalLendingPanel from "~~/components/LocalLendingPanel/LocalLendingPanel";
 import { useScaffoldContract, useScaffoldContractRead } from "~~/hooks/scaffold-eth";
+
+interface Pool {
+  address: string;
+  result: {
+    totalSupplied: number;
+    totalBorrowed: number;
+    name: string;
+  };
+  votingPower: number;
+}
 
 function MyPools() {
   const [showCheap, setShowCheap] = useState(false);
@@ -69,20 +79,27 @@ function MyPools() {
       });
 
       setPoolList(
-        filteredPools.map(pool => {
+        filteredPools.map((pool: Pool) => {
           const poolAddr = pool.address;
-          const availableLiquidity: bigint = pool.result.totalSupplied - pool.result.totalBorrowed;
-          const depositValue: bigint = (pool.votingPower * BigInt(pool.result.totalSupplied)) / BigInt(100);
+
+          const totalSupplied: bigint =
+            typeof pool.result.totalSupplied === "number" ? BigInt(pool.result.totalSupplied) : BigInt(0);
+          const totalBorrowed: bigint =
+            typeof pool.result.totalBorrowed === "number" ? BigInt(pool.result.totalBorrowed) : BigInt(0);
+          const votingPower: bigint = typeof pool.votingPower === "number" ? BigInt(pool.votingPower) : BigInt(0);
+
+          const availableLiquidity: bigint = BigInt(pool.result.totalSupplied) - BigInt(pool.result.totalBorrowed);
+          const depositValue: bigint = (BigInt(pool.votingPower) * BigInt(pool.result.totalSupplied)) / BigInt(100);
           return {
             address: poolAddr || "",
             name: pool.result ? pool.result.name : "",
             availableLiquidity: availableLiquidity ? formatUnits(availableLiquidity, 18) : 0,
-            votingPower: pool.votingPower,
+            votingPower: votingPower,
             depositValue: depositValue ? formatUnits(depositValue, 18) : 0,
             // borrowedAPY: "0%",
             // suppliedAPY: "0%",
-            totalBorrowed: pool.result ? formatUnits(pool.result.totalBorrowed, 18) : "0",
-            totalSupplied: pool.result ? formatUnits(pool.result.totalSupplied, 18) : "0",
+            totalBorrowed: totalBorrowed ? formatUnits(totalBorrowed, 18) : "0",
+            totalSupplied: totalSupplied ? formatUnits(totalSupplied, 18) : "0",
           };
         }),
       );
@@ -95,39 +112,53 @@ function MyPools() {
   };
 
   if (showCheap) {
-    return <Cheap address={selectedPool} setShowCheap={setShowCheap} />;
+    return <LocalLendingPanel address={selectedPool} setShowCheap={() => setShowCheap} />;
   }
 
   return (
     <>
       <h1 className="text-4xl text-[#4A5056] font-bold">My pools</h1>
       <div className="w-[1200px] flex flex-col">
-        <div className="flex gap-4 h-[95px] items-center p-8 border border-solid border-[#EAEBEF]">
-          <span className="w-[160px] text-center">Pool</span>
-          <span className="w-[160px] text-center">Available liquidity</span>
-          <span className="w-[160px] text-center">Deposit Value</span>
-          <span className="w-[160px] text-center">Voting Power</span>
-          <span className="w-[160px] text-center"></span>
-        </div>
-        {poolList.map(pool => (
-          <div key={pool.name} className="flex gap-4 h-[95px] items-center p-8 border border-solid border-[#EAEBEF]">
-            <p className="w-[160px] text-center">{pool.name}</p>
-            <div className="w-[160px] text-center flex gap-1 items-center justify-center">
-              <span className="">{pool.availableLiquidity}</span>
+        {poolsLoading ? (
+          <>
+            <div className="flex flex-col items-center justify-center gap-16 my-7">
+              <Image src="/loading.png" alt="loading" width={150} height={150}></Image>
+              <span className="text-3xl text-[#4A5056] font-semibold"> Loading </span>
             </div>
-            <p className="w-[160px] text-center">{pool.depositValue}</p>
-            <p className="w-[160px] text-center">{formatUnits(pool.votingPower, 0)} %</p>
-            <p className="w-[160px] text-center"></p>
-            <button
-              onClick={() => {
-                handleDetailsClick(pool.address);
-              }}
-              className="border border-solid border-[#4A5056] rounded-[7px] py-4 px-10"
-            >
-              Details
-            </button>
-          </div>
-        ))}
+          </>
+        ) : (
+          <>
+            <div className="flex gap-4 h-[95px] items-center p-8 border border-solid border-[#EAEBEF]">
+              <span className="w-[160px] text-center">Pool</span>
+              <span className="w-[160px] text-center">Available liquidity</span>
+              <span className="w-[160px] text-center">Deposit Value</span>
+              <span className="w-[160px] text-center">Voting Power</span>
+              <span className="w-[160px] text-center"></span>
+            </div>
+            {poolList.map(pool => (
+              <div
+                key={pool.name}
+                className="flex gap-4 h-[95px] items-center p-8 border border-solid border-[#EAEBEF]"
+              >
+                <p className="w-[160px] text-center">{pool.name}</p>
+                <div className="w-[160px] text-center flex gap-1 items-center justify-center">
+                  <span className="">{pool.availableLiquidity}</span>
+                </div>
+                <p className="w-[160px] text-center">{pool.depositValue}</p>
+                <p className="w-[160px] text-center">{formatUnits(pool.votingPower, 0)} %</p>
+                <p className="w-[160px] text-center"></p>
+                <button
+                  onClick={() => {
+                    handleDetailsClick(pool.address);
+                  }}
+                  className="border border-solid border-[#4A5056] rounded-[7px] py-4 px-10"
+                >
+                  {poolAddressesLoading ? "Loading" : "Details"}
+                </button>
+              </div>
+            ))}
+          </>
+        )}
       </div>
     </>
   );
--- packages/nextjs/components/Header.tsx
+++ packages/nextjs/components/Header.tsx
@@ -1,9 +1,12 @@
 "use client";
 
+import Image from "next/image";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
+import { formatEther, parseEther } from "viem";
+import { useAccount } from "wagmi";
 import { FaucetButton, RainbowKitCustomConnectButton } from "~~/components/scaffold-eth";
-import Image from "next/image";
+import { useScaffoldContractRead, useScaffoldContractWrite } from "~~/hooks/scaffold-eth";
 
 type HeaderMenuLink = {
   label: string;
@@ -44,13 +47,30 @@ export const HeaderMenuLinks = () => {
  * Site header
  */
 export const Header = () => {
+  const account = useAccount();
+
+  const { writeAsync: mint } = useScaffoldContractWrite({
+    contractName: "MockToken",
+    functionName: "mint",
+    args: [account.address, parseEther("1000")],
+  });
+
+  const { data: balance } = useScaffoldContractRead({
+    contractName: "MockToken",
+    functionName: "balanceOf",
+    args: [account.address],
+    watch: true,
+  });
+
+  const balanceDai = balance !== undefined ? formatEther(balance) : "Cargando...";
+
   return (
     <>
-      <div className="sticky top-0 z-20 justify-between flex-shrink-0 min-h-0 p-6 shadow-md lg:static navbar bg-base-100 sm:px-2">
+      <div className="sticky top-0 z-20 justify-between flex-shrink-0 min-h-0 p-6 bg-white shadow-md lg:static background-svg navbar sm:px-2 ">
         <div className="flex gap-3">
           <Link href="/" passHref className="items-center hidden gap-2 ml-4 mr-6 lg:flex shrink-0">
             <div>
-              <Image src="/logo.png" alt="logo" width={57} height={47}></Image>
+              <Image src="/logo.png" alt="logo" width={57} height={47} className="w-auto h-auto" priority></Image>
             </div>
           </Link>
           <ul className="hidden gap-8 px-1 text-xl lg:flex lg:flex-nowrap">
